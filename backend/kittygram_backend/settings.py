245799961f774c6c8d310f1a5f655fb321589c4b
ZeraiGR <kirillgr15@mail.ru>
2024-12-29 01:35:43 +0300

    fix python settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -67,6 +67,9 @@ DATABASES = {
     }
 }
 
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
